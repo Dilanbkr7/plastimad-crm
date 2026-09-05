f58c69b8d51db8e9b322ac547afd66fb1199ef49
Dilanbkr7 <dillanobandos@hotmail.com>
2026-09-04 19:43:51 -0500

    Agregar Meta Pixel tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 
 import "./globals.css";
+import MetaPixel from "@/components/tracking/MetaPixel";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -46,6 +47,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <MetaPixel />
+
         {children}
       </body>
     </html>
